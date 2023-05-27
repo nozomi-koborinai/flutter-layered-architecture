d8cbcbd4d0bcd160a32318e0895e701acc43e177
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-05-27 20:01:06 +0900

    🐛 fix: PostRepository の Mock の注入漏れ

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reference_app_2/domain/post/post_repository.dart';
 import 'package:flutter_reference_app_2/domain/service/storage_service.dart';
 import 'package:flutter_reference_app_2/domain/user/user_repository.dart';
+import 'package:flutter_reference_app_2/infrastructure/mocks/mock_post_repository.dart';
 import 'package:flutter_reference_app_2/infrastructure/mocks/mock_storage_service.dart';
 import 'package:flutter_reference_app_2/presentation/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,6 +14,7 @@ void main() {
     ProviderScope(
       overrides: [
         userRepositoryProvider.overrideWithValue(MockUserRepository()),
+        postRepositoryProvider.overrideWithValue(MockPostRepository()),
         storageServiceProvider.overrideWithValue(MockStorageService()),
       ],
       child: const App(),
