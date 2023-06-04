d00144643dcc91dcc136d9c3fefd0d4e00fad2e1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-04 20:53:30 +0900

    ✨ style: Mock メソッドのコメント追記
    Closes

--- lib/infrastructure/mocks/mock_storage_service.dart
+++ lib/infrastructure/mocks/mock_storage_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_reference_app_2/domain/service/storage_service.dart';
 
 class MockStorageService implements StorageService {
+  /// 指定画像を外部ストレージに保存した程にして、ダミーの保存先 URL を返却
   @override
   Future<String> uploadImage({required File image}) async {
     await Future.delayed(const Duration(seconds: 2));
