e7297ec07bac06906dbb12d293e0f9feea482364
masaki <masakisato.japan@gmail.com>
2023-07-09 15:37:47 +0900

    ✨add: addPostのテストを作成

--- test/post_usecase_test.dart
+++ test/post_usecase_test.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_layered_architecture/application/usecase/post/post_usecase.dart';
+import 'package:flutter_layered_architecture/domain/post/entity/post.dart';
 import 'package:flutter_layered_architecture/domain/post/post_repository.dart';
 import 'package:flutter_layered_architecture/domain/service/storage_service.dart';
+import 'package:flutter_layered_architecture/domain/user/entity/user.dart';
 import 'package:flutter_layered_architecture/infrastructure/mocks/mock_post_repository.dart';
 import 'package:flutter_layered_architecture/infrastructure/mocks/mock_storage_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  late final ProviderContainer container;
-  late final MockPostRepository repository;
+  late ProviderContainer container;
+  late MockPostRepository repository;
+  // late PostUsecase usecase;
 
   setUp(() {
     container = ProviderContainer(
@@ -17,8 +20,8 @@ void main() {
         storageServiceProvider.overrideWithValue(MockStorageService()),
       ],
     );
-    // TODO(masaki): 不要であれば削除
     repository = container.read(postRepositoryProvider) as MockPostRepository;
+    // usecase = container.read(postUsecaseProvider);
   });
 
   /// fetchAll用テスト
@@ -40,19 +43,46 @@ void main() {
     });
   });
 
-// TODO(masaki): add postもやってpost_usecase_testに全体にするか検討
-//   group('addPost', () {
-//     test('投稿が追加されている', () async {
-//       final usecase = container.read(postUsecaseProvider);
-//       final result = await usecase.addPost(
-//         title: 'test title',
-//         body: 'test body',
-//       );
-//
-//       final sortedPosts = repository.mockPosts
-//         ..sort((a, b) => b.createdAt!.compareTo(a.createdAt!));
-//
-//       expect(result, sortedPosts);
-//     });
-//   });
+  /// addPost用テスト
+  group('addPost', () {
+    /// 現在時点のタイムスタンプから生成したidを用いて、実際にレポジトリに投稿が追加されているのか確認する
+    test('投稿が追加されている', () async {
+      /// usecaseをテストする場合
+      ///
+      // TODO(masaki): 動かない理由を徳田さんに相談
+      // final userId = DateTime.now().microsecondsSinceEpoch.toString();
+      // final usecase = container.read(postUsecaseProvider);
+      //
+      // await usecase.addPost(
+      //   comment: '',
+      //   image: null,
+      //   user: User(
+      //     id: userId,
+      //     userName: '',
+      //     imageUrl: '',
+      //   ),
+      // );
+      //
+      // final posts = await usecase.fetchAll();
+      // final addedPost = posts.first;
+      //
+      // expect(addedPost.user.id, userId);
+
+      /// repositoryをテストする場合
+      // TODO(masaki): repositoryをテストする理由を徳田さんに相談
+
+      final id = DateTime.now().microsecondsSinceEpoch.toString();
+
+      await repository.add(
+          post: Post(
+              id: id,
+              user: const User(id: '', userName: '', imageUrl: ''),
+              imageUrl: '',
+              comment: '',
+              createdAt: DateTime.now()));
+      final addedPost = repository.mockPosts.last;
+
+      expect(addedPost.id, id);
+    });
+  });
 }
