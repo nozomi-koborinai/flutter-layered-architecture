39a0b51eb2aaf509e76b36da4575178cad2a9dcd
masaki <masakisato.japan@gmail.com>
2023-08-05 16:03:23 +0900

    🚀 feat: flavorでビルド時にレポジトリを切り替えられるようにした
    enumはおそらく今後リモート環境の切り替えで用いるが、一旦main.dart内のみでの利用なのでファイル内に置いている。今後別ファイルからも用いるのであれば、以下の選択肢を検討したい
    
    - domain内におく
    依存関係としては問題ないが役責的には違うので避けた方が良さそう
    - トップレベルにそのままおく
    ファイルが多くないのなら一旦これで良さそう
    - commonディレクトリを作る
    ファイルが増えてきたらこうした方が良さそう

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_layered_architecture/domain/service/storage_service.dart
 import 'package:flutter_layered_architecture/domain/user/user_repository.dart';
 import 'package:flutter_layered_architecture/infrastructure/mocks/mock_post_repository.dart';
 import 'package:flutter_layered_architecture/infrastructure/mocks/mock_storage_service.dart';
+import 'package:flutter_layered_architecture/infrastructure/remote/graph_ql_post_repository.dart';
+import 'package:flutter_layered_architecture/infrastructure/remote/graph_ql_storage_service.dart';
+import 'package:flutter_layered_architecture/infrastructure/remote/graph_ql_user_repository.dart';
 import 'package:flutter_layered_architecture/presentation/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,11 +16,28 @@ void main() {
   runApp(
     ProviderScope(
       overrides: [
-        userRepositoryProvider.overrideWithValue(MockUserRepository()),
-        postRepositoryProvider.overrideWithValue(MockPostRepository()),
-        storageServiceProvider.overrideWithValue(MockStorageService()),
+        userRepositoryProvider.overrideWithValue(
+            Flavor.isDemo ? MockUserRepository() : GraphQlUserRepository()),
+        postRepositoryProvider.overrideWithValue(
+            Flavor.isDemo ? MockPostRepository() : GraphQlPostRepository()),
+        storageServiceProvider.overrideWithValue(
+            Flavor.isDemo ? MockStorageService() : GraphQlStorageService()),
       ],
       child: const App(),
     ),
   );
+}
+
+/// ビルド時の引数を参照したサーバー環境切り替え用のenum
+enum Flavor {
+  demo,
+  // FIXME(masaki): リモート環境追加後に状況に合わせて随時修正&追加
+  dev;
+
+  static Flavor get fromEnvironment {
+    const flavorStr = String.fromEnvironment('flavor');
+    return Flavor.values.byName(flavorStr);
+  }
+
+  static bool get isDemo => fromEnvironment == demo;
 }
