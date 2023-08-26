d37f5e456961f444959962d3b89f755451b9e595
masaki <masakisato.japan@gmail.com>
2023-08-26 16:02:53 +0900

    ♻ refactor: post_usecaseにおいてrefを渡す実装に変更
    元々の完全にDIする形だとView側から毎度invalidateをする処理を呼ぶ必要があったが、役責的にはその処理はusecase側に任せたかったので、refを渡す&getterで依存関係を明示する、という運用にした

--- lib/presentation/page/post/component/post_button.dart
+++ lib/presentation/page/post/component/post_button.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_layered_architecture/application/usecase/post/post_usecase.dart';
-import 'package:flutter_layered_architecture/application/usecase/post/state/posts_provider.dart';
 import 'package:flutter_layered_architecture/application/usecase/user/state/user_provider.dart';
-import 'package:flutter_layered_architecture/presentation/presentation_mixin.dart';
 import 'package:flutter_layered_architecture/presentation/page/post/component/post_comment_field.dart';
 import 'package:flutter_layered_architecture/presentation/page/post/component/post_image.dart';
+import 'package:flutter_layered_architecture/presentation/presentation_mixin.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PostButton extends ConsumerWidget with PresentationMixin {
@@ -25,7 +24,6 @@ class PostButton extends ConsumerWidget with PresentationMixin {
                   user: ref.read(userProvider),
                 );
             navigator.pop();
-            ref.invalidate(postsProvider);
           },
           successMessage: '投稿が完了しました',
         );
